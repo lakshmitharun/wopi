c5cd25d61f300ed1f3b89c631e75f5c514d809b6
ponnl <lakshmitharun.ponnam@in.pega.com>
2019-02-18 15:16:59 +0530

    first commit

---
+++ src/main/java/com/pega/wopi/WopiApplication.java
@@ -0,0 +1,13 @@
+package com.pega.wopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WopiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WopiApplication.class, args);
+    }
+
+}
